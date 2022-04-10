36e867087ddbfdb09dad2ef47d701aac67dc340d
Guillermo Peitzner <guillermopeitzner@gmail.com>
2022-04-10 09:34:19 -0600

    [add] base files

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Input } from "@ui5/webcomponents-react";
+import React from "react";
+import "./App.css";
+
+function App() {
+	return <div></div>;
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "@ui5/webcomponents-react";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-
-root.render(<App />);
+ReactDOM.render(
+	<React.StrictMode>
+		<ThemeProvider>
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>,
+	document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
