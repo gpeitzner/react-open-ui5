d51f2f042a789c1a875f3d37f6ca45dc179c3857
Guillermo Peitzner <guillermopeitzner@gmail.com>
2022-04-12 19:45:47 -0600

    [add] button wrapper

---
+++ src/wrappers/button/button.wrapper.tsx
@@ -0,0 +1,33 @@
+import "./button.wrapper.css";
+
+import { Button } from "@ui5/webcomponents-react";
+
+export interface ButtonWrapperParameters {
+	text: string;
+	action: any;
+	design?:
+		| "Default"
+		| "Emphasized"
+		| "Positive"
+		| "Negative"
+		| "Transparent"
+		| "Attention";
+	disabled?: boolean;
+	icon?: string;
+}
+
+function ButtonWrapper(props: ButtonWrapperParameters) {
+	return (
+		<Button
+			className="button-wrapper"
+			onClick={props.action}
+			design={props.design !== undefined ? props.design : "Default"}
+			disabled={props.disabled !== undefined ? props.disabled : false}
+			icon={props.icon && props.icon}
+		>
+			{props.text}
+		</Button>
+	);
+}
+
+export default ButtonWrapper;
